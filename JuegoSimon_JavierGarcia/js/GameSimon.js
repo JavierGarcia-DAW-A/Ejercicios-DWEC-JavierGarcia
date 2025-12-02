958de02fd4880beee2fd931dcce5dd92b07c6e8a
JavierGarcia-DAW-A <jgarsani@ieszaidinvergeles.org>
2025-12-02 18:32:23 +0100

    Intentar arreglar el error de turnoJugador y empezar a plantear un poco el detector de voz

--- JuegoSimon_JavierGarcia/js/GameSimon.js
+++ JuegoSimon_JavierGarcia/js/GameSimon.js
@@ -25,37 +25,88 @@ export const GameSimon = {
         }
 
         for (let index of GameSimon.listaOrden) {
-            await GameSimon.turnoJugador(index);
-            index = index + 1;
+            const elemento = GameSimon.lista[index];
+            console.log(elemento.id);
+            await GameSimon.turnoJugador(elemento).then((valor) => {
+                console.log("Siguiente tecla");
+            }, (valor) => {
+                console.log("Has fallado");
+            });
         }
     },
 
 
     pulsarTecla: async (elemento) => {
         return new Promise((resolve) => {
-
             const tecla = document.getElementById(elemento.id);
             tecla.style.backgroundColor = elemento.colorOn;
 
             setTimeout(() => {
                 tecla.style.backgroundColor = elemento.colorOff;
-                resolve();
+                resolve(99);
             }, 1000);
         });
     },
 
-    turnoJugador: (index) => {
-        return new Promise((resolve) => {
+    turnoJugador: (elemento) => {
+        return new Promise((resolve, reject) => {
+            /*GameSimon.pruebaVoz();*/
             document.addEventListener('click', (e) => {
-                if (e.target.id === GameSimon.lista[index].id) {
+                if (e.target.id === elemento.id) {
                     console.log("Voy bien");
+                    resolve(99);
                 } else {
-                    console.log(GameSimon.lista[index].id);
+                    console.log(elemento.id);
                     console.log(e.target.id);
                     console.log("Mal, fatal, horrible");
+                    reject(34)
                 }
+
+
             })
         })
+    },
+
+    /*
+    pruebaVoz: () => {
+        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+        var SpeechRecognitionEvent =
+        SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+        var colors = [
+        "blue",
+        "green",
+        "red",
+        "yellow",
+        ];
+
+        const recognition = new SpeechRecognition();
+        recognition.continuous = false;
+        recognition.lang = "en-US";
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+
+        recognition.start();
+        console.log("Ready to receive a color command.");
+
+        recognition.onresult = function (event) {
+        const color = event.results[0][0].transcript;
+        console.log(color);
+        };
+
+        recognition.onspeechend = function () {
+        recognition.stop();
+        };
+
+        recognition.onnomatch = function (event) {
+        diagnostic.textContent = "I didn't recognise that color.";
+        };
+
+        recognition.onerror = function (event) {
+        diagnostic.textContent = "Error occurred in recognition: " + event.error;
+        };
     }
+
+    */
 }
 
