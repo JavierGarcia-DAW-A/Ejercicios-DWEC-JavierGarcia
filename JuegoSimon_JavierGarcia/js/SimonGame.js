627ec55bfbbd3a6afc88b22c4999d75b2fed4289
JavierGarcia-DAW-A <jgarsani@ieszaidinvergeles.org>
2025-12-08 11:47:18 +0100

    Intento de implementación de voz

--- JuegoSimon_JavierGarcia/js/SimonGame.js
+++ JuegoSimon_JavierGarcia/js/SimonGame.js
@@ -62,47 +62,5 @@ export class JuegoSimon {
             }
         }
     };
-
-    /*
-    pruebaVoz: () => {
-        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-        var SpeechRecognitionEvent =
-        SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-        var colors = [
-        "blue",
-        "green",
-        "red",
-        "yellow",
-        ];
-
-        const recognition = new SpeechRecognition();
-        recognition.continuous = false;
-        recognition.lang = "en-US";
-        recognition.interimResults = false;
-        recognition.maxAlternatives = 1;
-
-        recognition.start();
-        console.log("Ready to receive a color command.");
-
-        recognition.onresult = function (event) {
-        const color = event.results[0][0].transcript;
-        console.log(color);
-        };
-
-        recognition.onspeechend = function () {
-        recognition.stop();
-        };
-
-        recognition.onnomatch = function (event) {
-        diagnostic.textContent = "I didn't recognise that color.";
-        };
-
-        recognition.onerror = function (event) {
-        diagnostic.textContent = "Error occurred in recognition: " + event.error;
-        };
-    }
-
-    */
 }
 
